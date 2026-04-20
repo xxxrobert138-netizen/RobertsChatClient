438f354859c0324ff0196c94a907cb36971841d2
fffROBERTfff <xxxrobert138@gmail.com>
2026-04-20 17:28:55 +0300

    new design

--- app/src/main/java/com/example/robertschat/MainActivity.java
+++ app/src/main/java/com/example/robertschat/MainActivity.java
@@ -1,58 +1,72 @@
 package com.example.robertschat;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textViewChat;
     private EditText editText;
     private WebSocket webSocket;
     private OkHttpClient client;
+    private List<String> messages = new ArrayList<>();
+    private ListView listView;
+    private static final String KEY_FOR_CAR_LIST = "carlist";
+    private ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        String r = result.getData().getStringExtra("name");
+        if (r == null) return;
+        messages.add(r);
+        updateListView();
+    });
 
+    private ActivityResultLauncher<Intent> launcherToCarInformatio = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        Toast.makeText(this, "jtijrjtrjejit", Toast.LENGTH_SHORT).show();
+    });
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewChat = findViewById(R.id.textView);
-        editText = findViewById(R.id.editTextText);
-
+        listView = findViewById(R.id.listCars);
+        updateListView();
+        editText = findViewById(R.id.editTextView);
         client = new OkHttpClient();
         startWebSocket();
     }
 
     private void startWebSocket() {
         Request request = new Request.Builder()
-                .url("wss://robertschatserver.onrender.com/")
+                .url("https://robertschatserver.onrender.com")
                 .build();
 
         webSocket = client.newWebSocket(request, new WebSocketListener() {
             @Override
             public void onMessage(@NotNull WebSocket webSocket, @NotNull String text) {
-                runOnUiThread(() -> textViewChat.setText(text));
-            }
-
-            @Override
-            public void onFailure(@NotNull WebSocket webSocket, @NotNull Throwable t, Response response) {
-                t.printStackTrace();
+                runOnUiThread(() -> {
+                    messages.add(text);
+                    updateListView();
+                });
             }
         });
-    }
-
-    public void onPressButtonUpdate(View view) {
-        if (webSocket != null) webSocket.close(1000, null);
-        startWebSocket();
     }
 
     public void onPressButtonSend(View view) {
@@ -69,5 +83,10 @@ public class MainActivity extends AppCompatActivity {
         if (webSocket != null) {
             webSocket.close(1000, null);
         }
+    }
+
+    private void updateListView() {
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, messages);
+        listView.setAdapter(arrayAdapter);
     }
 }
